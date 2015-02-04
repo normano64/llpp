de00527de27b0bc65456e9ad4a32720a755b81e5
Normano64 <per.bergqwist@gmail.com>
2015-02-04 15:52:00 +0100

    Tog bort onödig rad och lite till

--- lab1/demo/ploter.sh
+++ lab1/demo/ploter.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=.
 rm -f ./testdata.txt
 RUNS=2
 THREADS=4
@@ -9,17 +8,18 @@ if [[ $1 =~ ^-?[0-9]+$ ]] && [ $1 > 0 ]; then
         THREADS=$2
     fi
 fi
-printf "Running %d runs with 1-%d threads.\n" $RUNS $THREADS 
+printf "Test of %d runs with 1-%d threads.\n" $RUNS $THREADS 
 for i in $(seq 1 $THREADS);
 do
-    printf "Running with %d threads" $i
     if [ "$i" == 1 ]; then
+        printf "Running with %d thread" $i
         for n in $(seq 1 $RUNS);
         do
             printf .
             LD_LIBRARY_PATH=. ./demo --silent --timing-mode --plot --threads 1
         done
     else
+        printf "Running with %d threads" $i
         for n in $(seq 1 $RUNS);
         do
             printf .
