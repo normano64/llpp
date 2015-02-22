70073cc1ebe642679d6c9138ec1700eb76915efc
Normano64 <per.bergqwist@gmail.com>
2015-02-22 03:01:09 +0100

    OpenCL with now async readbuffer and report

--- lab2/demo/ploter.sh
+++ lab2/demo/ploter.sh
@@ -11,32 +11,60 @@ fi
 printf "Test of %d runs with 1-%d threads.\n" $RUNS $THREADS 
 for i in $(seq 1 $THREADS);
 do
+    printf "Running with %d threads" $i
     if [ "$i" == 1 ]; then
-        printf "Running with %d thread" $i
         rm -f ./testdata.txt
         for n in $(seq 1 $RUNS);
         do
             printf .
-            LD_LIBRARY_PATH=. ./demo --silent --timing-mode --plot --threads 1
+            LD_LIBRARY_PATH=. ./demo --silent --timing-mode --plot --threads "$i"
         done
         FRACTION="$(awk '{total += $6} END {total=total/'$RUNS';print total}' testdata.txt)"
-    else
-        printf "Running with %d threads" $i
+        awk '{total += $6} END {total=(total/'$RUNS');print "seq     \t\t('$i',"total")"}' testdata.txt >> data.txt
         rm -f ./testdata.txt
         for n in $(seq 1 $RUNS);
         do
             printf .
-            LD_LIBRARY_PATH=. ./demo --silent --timing-mode --pthread --plot --threads "$i"
+            LD_LIBRARY_PATH=. ./demo --silent --timing-mode --plot --threads "$i" --simd
         done
-        awk '{total += $6} END {total=(total/'$RUNS')/'$FRACTION';print "pthread\t('$i',"total")"}' testdata.txt >> data.txt
+        awk '{total += $6} END {total=(total/'$RUNS')/'$FRACTION';print "seq     \tsimd\t('$i',"total")"}' testdata.txt >> data.txt
         rm -f ./testdata.txt
         for n in $(seq 1 $RUNS);
         do
             printf .
-            LD_LIBRARY_PATH=. ./demo --silent --timing-mode --omp --plot --threads "$i"
+            LD_LIBRARY_PATH=. ./demo --silent --timing-mode --opencl --plot
         done
-        awk '{total += $6} END {total=(total/'$RUNS')/'$FRACTION';print "omp\t('$i',"total")"}' testdata.txt >> data.txt
+        awk '{total += $6} END {total=(total/'$RUNS')/'$FRACTION';print "opencl  \t\t('$i',"total")"}' testdata.txt >> data.txt
     fi
+    rm -f ./testdata.txt
+    for n in $(seq 1 $RUNS);
+    do
+        printf .
+        LD_LIBRARY_PATH=. ./demo --silent --timing-mode --pthreads --plot --threads "$i"
+    done
+    awk '{total += $6} END {total=(total/'$RUNS')/'$FRACTION';print "pthreads\t\t('$i',"total")"}' testdata.txt >> data.txt
+    rm -f ./testdata.txt
+    for n in $(seq 1 $RUNS);
+    do
+        printf .
+        LD_LIBRARY_PATH=. ./demo --silent --timing-mode --pthreads --plot --threads "$i" --simd
+    done
+    awk '{total += $6} END {total=(total/'$RUNS')/'$FRACTION';print "pthreads\tsimd\t('$i',"total")"}' testdata.txt >> data.txt
+    rm -f ./testdata.txt
+    for n in $(seq 1 $RUNS);
+    do
+        printf .
+        LD_LIBRARY_PATH=. ./demo --silent --timing-mode --omp --plot --threads "$i"
+    done
+    awk '{total += $6} END {total=(total/'$RUNS')/'$FRACTION';print "omp     \t\t('$i',"total")"}' testdata.txt >> data.txt
+    rm -f ./testdata.txt
+    for n in $(seq 1 $RUNS);
+    do
+        printf .
+        LD_LIBRARY_PATH=. ./demo --silent --timing-mode --omp --plot --threads "$i" --simd
+    done
+    awk '{total += $6} END {total=(total/'$RUNS')/'$FRACTION';print "omp     \tsimd\t('$i',"total")"}' testdata.txt >> data.txt
+    rm -f ./testdata.txt
     echo ""
 done
 echo "Tests complete!"
