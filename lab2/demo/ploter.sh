94000a41ad4dcde179df851231d706a75b559fe4
Normano64 <per.bergqwist@gmail.com>
2015-02-22 19:54:43 +0100

    2 Plots now

--- lab2/demo/ploter.sh
+++ lab2/demo/ploter.sh
@@ -17,7 +17,7 @@ do
         for n in $(seq 1 $RUNS);
         do
             printf .
-            LD_LIBRARY_PATH=. ./demo --silent --timing-mode --plot --threads "$i"
+            LD_LIBRARY_PATH=. ./demo scenario_small.xml --silent --timing-mode --plot --threads "$i"
         done
         FRACTION="$(awk '{total += $6} END {total=total/'$RUNS';print total}' testdata.txt)"
         awk '{total += $6} END {total=(total/'$RUNS');print "seq     \t\t('$i',"total")"}' testdata.txt >> data.txt
@@ -25,14 +25,14 @@ do
         for n in $(seq 1 $RUNS);
         do
             printf .
-            LD_LIBRARY_PATH=. ./demo --silent --timing-mode --plot --threads "$i" --simd
+            LD_LIBRARY_PATH=. ./demo scenario_small.xml --silent --timing-mode --plot --threads "$i" --simd
         done
         awk '{total += $6} END {total=(total/'$RUNS')/'$FRACTION';print "seq     \tsimd\t('$i',"total")"}' testdata.txt >> data.txt
         rm -f ./testdata.txt
         for n in $(seq 1 $RUNS);
         do
             printf .
-            LD_LIBRARY_PATH=. ./demo --silent --timing-mode --opencl --plot
+            LD_LIBRARY_PATH=. ./demo scenario_small.xml --silent --timing-mode --opencl --plot
         done
         awk '{total += $6} END {total=(total/'$RUNS')/'$FRACTION';print "opencl  \t\t('$i',"total")"}' testdata.txt >> data.txt
     fi
@@ -40,28 +40,28 @@ do
     for n in $(seq 1 $RUNS);
     do
         printf .
-        LD_LIBRARY_PATH=. ./demo --silent --timing-mode --pthreads --plot --threads "$i"
+        LD_LIBRARY_PATH=. ./demo scenario_small.xml --silent --timing-mode --pthreads --plot --threads "$i"
     done
     awk '{total += $6} END {total=(total/'$RUNS')/'$FRACTION';print "pthreads\t\t('$i',"total")"}' testdata.txt >> data.txt
     rm -f ./testdata.txt
     for n in $(seq 1 $RUNS);
     do
         printf .
-        LD_LIBRARY_PATH=. ./demo --silent --timing-mode --pthreads --plot --threads "$i" --simd
+        LD_LIBRARY_PATH=. ./demo scenario_small.xml --silent --timing-mode --pthreads --plot --threads "$i" --simd
     done
     awk '{total += $6} END {total=(total/'$RUNS')/'$FRACTION';print "pthreads\tsimd\t('$i',"total")"}' testdata.txt >> data.txt
     rm -f ./testdata.txt
     for n in $(seq 1 $RUNS);
     do
         printf .
-        LD_LIBRARY_PATH=. ./demo --silent --timing-mode --omp --plot --threads "$i"
+        LD_LIBRARY_PATH=. ./demo scenario_small.xml --silent --timing-mode --omp --plot --threads "$i"
     done
     awk '{total += $6} END {total=(total/'$RUNS')/'$FRACTION';print "omp     \t\t('$i',"total")"}' testdata.txt >> data.txt
     rm -f ./testdata.txt
     for n in $(seq 1 $RUNS);
     do
         printf .
-        LD_LIBRARY_PATH=. ./demo --silent --timing-mode --omp --plot --threads "$i" --simd
+        LD_LIBRARY_PATH=. ./demo scenario_small.xml --silent --timing-mode --omp --plot --threads "$i" --simd
     done
     awk '{total += $6} END {total=(total/'$RUNS')/'$FRACTION';print "omp     \tsimd\t('$i',"total")"}' testdata.txt >> data.txt
     rm -f ./testdata.txt
